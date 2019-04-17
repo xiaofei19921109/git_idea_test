4edbc0eee63f75982a0ffc1adebd199c6bb415d6
xiaofei_global <xiaofei_global@163.com>
2019-04-17 14:24:24 +0800

    add print

--- src/com/tw/HelloTest.java
+++ src/com/tw/HelloTest.java
@@ -2,6 +2,12 @@ package com.tw;
 
 public class HelloTest {
     public static void main(String[] args) {
+
         System.out.println("hello world，XiaoFei");
+
+    }
+
+    public void print(){
+        System.out.println("edit by wildestDream");
     }
 }
