3554e32d304524082bcd2ec21843aee86c3efcdf
xiaofei_global <xiaofei_global@163.com>
2019-04-17 14:52:06 +0800

    add plugin

---
+++ src/com/tw/com/pluginmall/Pluginmall.java
@@ -0,0 +1,14 @@
+package com.tw.com.pluginmall;
+
+/**
+ * @author 81976
+ * @title: Pluginmall
+ * @projectName git_test
+ * @description: TODO
+ * @date 2019-4-1714:49
+ */
+public class Pluginmall {
+    public void loadPlugins(){
+
+    }
+}
